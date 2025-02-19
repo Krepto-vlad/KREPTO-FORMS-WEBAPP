db30e2283b554a70357e2d4919e53574abab8587
Vlad <vladshokoladkin@gmail.com>
2025-02-19 19:42:39 +0200

    some fixes

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -2,9 +2,25 @@ import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import searchBtn from '../../assets/search.png'
 import logo from '../../assets/TESTfirstRED2.ico'
+import axiosClient from "../../api/axiosClient";
+import { AxiosErrorResponse } from "../../types/authTypes";
 
 const Navbar = () => {
 
+     const registerUser = async () => {
+        try {
+          const response = await axiosClient.get("/test");
+          console.log("getting data");
+          return response; // Возвращает и user, и token
+        } catch (err) {
+            console.log("in err");
+            
+          const error = err as AxiosErrorResponse;
+          throw new Error(error.response?.data?.message || "Registration failed");
+        }
+      };
+      console.log(registerUser());
+      
     return (
         <div className="navbar">
             <a href=""><img className="logo" src={logo} alt="" /></a>
@@ -13,9 +29,12 @@ const Navbar = () => {
                     <input type="text" className="search-input" placeholder="Type to Search..."/>
             </div>
 
+            
+
             <Link to="/login">
                 <button className="login-btn">Login</button>
             </Link>
+
         </div>
     )
 }
