6cd86e97995f21a3b6d4bfaedea39452f3be7134
Vlad <vladshokoladkin@gmail.com>
2025-02-25 01:01:49 +0200

    style fix

--- src/components/editForm/editForm.tsx
+++ src/components/editForm/editForm.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { deleteForm } from "../../api/formService";
+import "./editForm.scss";
 import axios from "axios";
 
 interface Form {
